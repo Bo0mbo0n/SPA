05676ef3b5d57d69059d0a6fed7cfb031d9d981b
Gustavo Contreras <qwertygussaints@gmail.com>
2018-08-07 22:44:46 -0600

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Yanet";
+  nombre1 = "IVONE YANET ESTRADA BUENDIA";
+
+  arreglo = [10,1,2,3,26,5,6,7,8,9];
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  minion = {
+    nombre:"Bob",
+    clave:"Despicable me",
+    edad:18,
+    direccion:{
+      calle:"Villa Residencial",
+      casa:"Gru"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject ) =>{
+
+    setTimeout( ()=> resolve('Llego info!'),3500);
+
+  })
+
+  fecha = new Date();
+
+  video = "mOeSfOJrUIk";
+
+ //mostrar = true;
+  //contrasena= activar;
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NamePipe } from './pipes/capitalizado.pipe';
+
+
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+registerLocaleData(localeEs);
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NamePipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:"es"}
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
